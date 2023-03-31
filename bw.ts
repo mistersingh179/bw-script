a9f3796e17fe86f14746397288178c05d9e8c1bc
Mister Singh <mistersingh179@gmail.com>
2023-03-31 17:08:27 -0400

    added a POST call in init to run on load
    this verified that CORS is working
    and that we are calling our server

--- bw.ts
+++ bw.ts
@@ -3,3 +3,13 @@ declare var BW_DASHBOARD_BASE_URL: string;
 const dashboardBaseUrl = BW_DASHBOARD_BASE_URL;
 
 console.log("i am bw.js: ", dashboardBaseUrl);
+
+const init = async () => {
+  const res = await fetch(`${dashboardBaseUrl}/api/auctions`, {
+    method: "POST"
+  });
+  const data = await res.json();
+  console.log(data);
+};
+
+init();

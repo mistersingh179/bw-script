02d5838490409acd726c5fcff1e646f1db1eba85
Mister Singh <mistersingh179@gmail.com>
2023-08-15 14:13:19 -0400

    added metaContent4 file which has more fightbookmma stuff

--- data/getMetaContent.ts
+++ data/getMetaContent.ts
@@ -1,16 +1,20 @@
 import metaContent from "./metaContent.json";
 import metaContent2 from "./metaContent2.json";
 import metaContent3 from "./metaContent3.json";
+import metaContent4 from "./metaContent4.json";
 
 export type MetaContentType = {
-  url: string,
-  input: string,
-  output: string[],
-  content_type?: string
-}
+  url: string;
+  input: string;
+  output: string[];
+  content_type?: string;
+};
 
 const getMetaContent = () => {
-  const meta = metaContent.concat(metaContent2).concat(metaContent3);
+  const meta = metaContent
+    .concat(metaContent2)
+    .concat(metaContent3)
+    .concat(metaContent4);
   return meta as MetaContentType[];
 };
 
@@ -19,6 +23,7 @@ export const getMetaContentUrls = () => {
   let metaContentUrls = metaContent.map((x) => x.url);
   metaContentUrls.push("http://localhost:3000/mma2.html");
   metaContentUrls.push("http://localhost:3000/mma3.html");
+  metaContentUrls.push("http://localhost:3000/mma4.html");
   metaContentUrls.push("http://localhost:3000/study2.html");
   metaContentUrls.push("http://localhost:3000/study3.html");
   metaContentUrls = [...new Set(metaContentUrls)];
